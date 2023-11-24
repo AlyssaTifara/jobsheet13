e24532774bd45785cbb953555e37e4d54cbb9389
Alyssa Tifara Yuwono <tissaalyssa@gmail.com>
2023-11-24 22:41:28 +0700

    percobaan2

--- TerimaKasih_3.java
+++ TerimaKasih_3.java
@@ -1,17 +1,14 @@
-import java.util.Scanner;
-public class TerimaKasih_3{
-    public static void main(String[] args) {
-        // Tidak menggunakan fungsi
+public class TerimaKasih_3 {
+    public static void UcapanTerimakasih() {
         System.out.println("Thank you for being the best teacher in the world.\n" +
-                "You inspired in me a love for learning and made me feel like I could ask you anything.");
-
-        // Menggunakan fungsi 
-        UcapanTerimaKasih();
+        "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
-
-    public static void UcapanTerimaKasih(){
-
-        System.out.println("Thank you for being the best teacher in the world.\n" +
-                "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    public static void UcapanTahmbahan (String ucapan){
+        System.out.println(ucapan);
+    }
+    public static void main (String[] args) {
+        UcapanTerimakasih();
+        String ucapan = "Terimakasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTahmbahan(ucapan);
     }
 }
