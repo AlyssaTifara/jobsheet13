c749f6a2bfed0834189b21f526da6da9c5b61cbf
Alyssa Tifara Yuwono <tissaalyssa@gmail.com>
2023-11-25 11:30:33 +0700

    percobaan4_soal

--- UcapanTerimakasih_3.java
+++ UcapanTerimakasih_3.java
@@ -4,13 +4,19 @@ public class UcapanTerimakasih_3{
         Scanner sc = new Scanner(System.in);
         System.out.println("Tuliskan NAMA orang yang ingin anda beri ucapkan :");
         String namaOrang = sc.nextLine();
-        sc.close();
         return namaOrang;
     }
-    public static void UcapanTerimakasih(){
+    public static String UcapanTambahan() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tuliskan apa yang ingin anda ucapkan pada orang tersebut :");
+        String ucapanTambahan = sc.nextLine();
+        return ucapanTambahan;
+    }
+
+    public static void UcapanTerimakasih() {
         String nama = PenerimaUcapan();
-        System.out.println("Thank you " +nama+ " for being the best teacher in the world.\n"+
-        "You inspired in me a love for learning and made me feel like i could ask you anything");
+        String tambahan = UcapanTambahan();
+        System.out.println("Thank you " + nama + ". " + tambahan);
     }
     public static void main(String[] args){
         UcapanTerimakasih();
